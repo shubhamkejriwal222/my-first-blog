144f2517ead4977db93b03bf06a4c59a36c0ef1f
Shubham Kejriwal <shubham.kejriwal222@gmail.com>
2018-09-18 18:14:26 +0530

    Name of Function changed

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .forms import PostForm
 from django.shortcuts import redirect
 
 # Create your views here.
-def post_list(request):
+def post_list1(request):
     posts =  Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
